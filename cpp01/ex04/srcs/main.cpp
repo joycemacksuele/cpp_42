76e0feec38da3591775264dc0222f48823d35a3b
Joyce Freitas <jfreitas@f0r4s7.codam.nl>
2022-01-27 18:16:07 +0100

    x01 ex04 almost done

--- cpp01/ex04/srcs/main.cpp
+++ cpp01/ex04/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jfreitas <jfreitas@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 14:42:17 by jfreitas          #+#    #+#             */
-/*   Updated: 2022/01/26 00:01:02 by jfreitas         ###   ########.fr       */
+/*   Updated: 2022/01/27 18:14:50 by jfreitas      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <new>
 #include <unistd.h>
 #include <fstream>
+
 /*
  * You are allowed to use everything in the standard library. HOWEVER, it
  * would be smart to try and use the C++-ish versions of the functions you
@@ -39,7 +40,8 @@ void	replace(std::string newFileName, std::string s1, std::string s2) {
 	i = 0;
 	newFile.open(newFileName, std::fstream::in);
 	if (newFile.is_open()) {
-		while (newFile) {
+		while (1) {
+			std::cout << "test";
 			if (newFile.eof())
 				break ;
 			std::getline(newFile, line);
@@ -48,44 +50,60 @@ void	replace(std::string newFileName, std::string s1, std::string s2) {
 			if (found != std::string::npos) {
 				i = 0;
 				while (found < line.size() && (line[found] != '\n' || line[found] != '\0')) {
-					std::cout << line[found] << "|" << s2[i] << std::endl;
 					if (s2[i]) {
+						std::cout << line[found] << "|" << s2[i] << std::endl;
 						line[found] = s2[i];
-						std::cout << line  << std::endl;
 						newFile << line;//something wrong here
 					}
 					i++;
 					found++;
 				}
+				std::cout << line  << std::endl;
 			}
+
 		}
 	}
 	newFile.close();
 }
 
-std::string	openAndCopyFile(std::string fileName) {
+std::string	openAndCopyFile(std::string fileName, std::string s1, std::string s2) {
 	std::fstream	file;
 	std::fstream	newFile;
 	std::string		newFileName;
+	std::string		line;
 	char			c;
+	int				i = 0;
 
-	newFileName = fileName + ".replace";
-	file.open(fileName, std::fstream::in);
-	newFile.open(newFileName, std::fstream::out);
-	if (file.is_open() && newFile.is_open()) {
-		while (1) {
-			c = file.get();
-			if (file.eof())
-				break ;
-			newFile << c;
+	file.open(fileName, std::fstream::in);// in for reading
+	if (file.is_open()) {
+		newFileName = fileName + ".replace";
+		newFile.open(newFileName, std::fstream::in | std::fstream::out);//in for reading and out for writing
+		if (newFile.is_open()) {
+			while (1) {
+				c = file.get();
+				//std::cout << c;
+				if (file.eof())
+					break ;
+				if (c == '\n' || c == '\0') {
+					while (1) {
+						std::getline(newFile, line);
+						size_t found = line.find(s1);
+						if (found != std::string::npos) {
+							i = 0;
+							while (found < line.size()) {
+								if (s2[i]) {
+									line[found] = s2[i];
+									newFile << line;
+								}
+							}
+						}
+					}
+					newFile << c;
+				}
+			}
 		}
 	}
 	file.close();
-
-	//std::string		line;
-	//std::getline(newFile, line);
-	//std::cout << "--" << line << std::endl;
-
 	newFile.close();
 	return newFileName;
 }
@@ -104,8 +122,8 @@ int		main(int argc, char **argv) {
 
 		if (errorRandling(argv[2], argv[3]))
 			return -1;
-		newFileName = openAndCopyFile(argv[1]);
-		replace(newFileName, argv[2], argv[3]);
+		newFileName = openAndCopyFile(argv[1], argv[2], argv[3]);
+		//replace(newFileName, argv[2], argv[3]);
 	}
 	return 0;
 }
