32ebd7661842d651753a7b807936e3f627ed4ec0
Joyce Freitas <jfreitas@f1r6s2.codam.nl>
2022-02-04 17:17:59 +0100

    x01 ex04 done

--- cpp01/ex04/includes/Replace.hpp
+++ cpp01/ex04/includes/Replace.hpp
@@ -6,7 +6,7 @@
 /*   By: jfreitas <jfreitas@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 13:39:52 by jfreitas          #+#    #+#             */
-/*   Updated: 2022/01/21 11:26:21 by jfreitas         ###   ########.fr       */
+/*   Updated: 2022/02/04 15:54:04 by jfreitas      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 #include <string>
 #include <iostream>
-#include <new>
-#include <unistd.h>
 #include <fstream>
 
 #define RESET   "\033[0m"
@@ -24,22 +22,5 @@
 #define GREEN   "\033[32m"
 #define YELLOW  "\033[33m"
 #define BLUE    "\033[34m"
-
-class Replace {
-
-	private:
-		std::string	filename;
-		std::string	s1;
-		std::string	s2;
-
-	public:
-		Replace( const std::string &s1,  const std::string &s2 );
-		~Replace( void );
-
-		std::string	gets1() const;
-		std::string	gets2() const;
-		void		setS1( std::string str1 );
-		void		setS2( std::string str2 );
-};
 
 #endif
--- cpp01/ex04/srcs/main.cpp
+++ cpp01/ex04/srcs/main.cpp
@@ -6,23 +6,11 @@
 /*   By: jfreitas <jfreitas@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 14:42:17 by jfreitas          #+#    #+#             */
-/*   Updated: 2022/02/04 07:56:00 by jfreitas         ###   ########.fr       */
+/*   Updated: 2022/02/04 15:39:32 by jfreitas      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "../includes/Replace.hpp"
-#include <string>
-#include <iostream>
-#include <new>
-#include <unistd.h>
-#include <fstream>
-
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-
+#include "../includes/Replace.hpp"
 
 /*
  * You are allowed to use everything in the standard library. HOWEVER, it
@@ -37,14 +25,12 @@
  * ----------------------------------------------------------------------------
  * All the member functions of the class std::string are allowed, except replace.
  */
-
-
 void	copyReplace(std::fstream &newFile, std::string line, std::string s1, std::string s2) {
 	size_t	linePos = 0;
 	size_t	i = 0;
 
 	if (line.find(s1, linePos) == std::string::npos) {
-		newFile << line << std::endl;
+		newFile << line;
 		return ;
 	}
 	while ((linePos = line.find(s1, i)) != std::string::npos) {
@@ -52,7 +38,7 @@ void	copyReplace(std::fstream &newFile, std::string line, std::string s1, std::s
 			newFile << line[i];
 			i++;
 		}
-		for (size_t j = 0; j < s2.size(); j++) {// putitng s2 in line
+		for (size_t j = 0; j < s2.size(); j++) {
 			newFile << s2[j];
 		}
 		i = i + s1.size();
@@ -76,27 +62,6 @@ void	openReadCopyReplaceFile(std::string fileName, std::string s1, std::string s
 		if (newFile.is_open()) {
 			while (std::getline(file, line)) {
 				copyReplace(newFile, line, s1, s2);
-
-			/*	linePos = 0;
-				i = 0;
-				if (line.find(s1, linePos) == std::string::npos) {
-					newFile << line << std::endl;
-					continue ;
-				}
-				while ((linePos = line.find(s1, i)) != std::string::npos) {
-					while (i < linePos) {
-						newFile << line[i];
-						i++;
-					}
-					for (size_t j = 0; j < s2.size(); j++) {// putitng s2 in line
-						newFile << s2[j];
-					}
-					i = i + s1.size();
-				}
-				while (i < line.size()) {
-					newFile << line[i];
-					i++;
-				}*/
 				newFile << std::endl;
 			}
 		}
