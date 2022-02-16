0902ce11d9980ce9eeaa4a7b83c78560af0253a3
Joyce Macksuele <60663410+joycemacksuele@users.noreply.github.com>
2022-02-16 15:14:45 +0100

    Update ClassPhonebook.cpp

--- cpp00/ex01/srcs/ClassPhonebook.cpp
+++ cpp00/ex01/srcs/ClassPhonebook.cpp
@@ -27,19 +27,24 @@ void	Phonebook::add(void) {
 
 	std::cout << "\t First name . . . . . ";
 	std::getline(std::cin, input);
-	_firstName = input;
+	//_firstName = input;
+	this->_contact.setFirstName(input);
 	std::cout << "\t Last name  . . . . . ";
 	std::getline(std::cin, input);
-	_lastName = input;
+	//_lastName = input;
+	this->_contact.setLastName(input);
 	std::cout << "\t Nickname . . . . . . ";
 	std::getline(std::cin, input);
-	_nickName = input;
+	//_nickName = input;
+	this->_contact.setNickName(input);
 	std::cout << "\t Phone number . . . . ";
 	std::getline(std::cin, input);
-	_phoneNumber = input;
+	//_phoneNumber = input;
+	this->_contact.setPhoneNumber(input);
 	std::cout << "\t Darkest secret . . . ";
 	std::getline(std::cin, input);
-	_darkestSecret = input;
+	//_darkestSecret = input;
+	this->_contact.setDarkestSecret(input);
 	return ;
 }
 
@@ -49,29 +54,32 @@ void	Phonebook::add(void) {
  * substr: https://www.cplusplus.com/reference/string/string/substr/
  */
 void	Phonebook::displayContactsList(int index) {
-	if (_firstName.empty() && _lastName.empty() && _nickName.empty() && _phoneNumber.empty() && _darkestSecret.empty())
+	//if (_firstName.empty() && _lastName.empty() && _nickName.empty() && _phoneNumber.empty() && _darkestSecret.empty())
+		//return ;
+	if (this->_contact.getFirstName().empty() && this->_contact.getLastName().empty() && this->_contact.getNickName().empty() &&
+	    this->_contact.getPhoneNumber().empty() && this->_contact.getDarkestSecret().empty())
 		return ;
 	std::cout << std::endl << "         │";
 	std::cout << std::setfill(' ') << std::setw(10);
 	std::cout << index << "│";
 	std::cout << std::setfill(' ') << std::setw(10);
-	_firstName.length() > 10 ? std::cout << _firstName.substr(0, 9) + '.' : std::cout << std::right << _firstName;
+	_firstName.length() > 10 ? std::cout << this->_contact.getFirstName().substr(0, 9) + '.' : std::cout << std::right << this->_contact.getFirstName();//_firstName
 	std::cout << "│";
 	std::cout << std::setfill(' ') << std::setw(10);
-	_lastName.length() > 10 ? std::cout << _lastName.substr(0, 9) + '.' : std::cout << std::right << _lastName;
+	_lastName.length() > 10 ? std::cout << this->_contact.getLastName().substr(0, 9) + '.' : std::cout << std::right << this->_contact.getLastName();//_lastName;
 	std::cout << "│";
 	std::cout << std::setfill(' ') << std::setw(10);
-	_nickName.length() > 10 ? std::cout << _nickName.substr(0, 9) + '.' : std::cout << std::right << _nickName;
+	_nickName.length() > 10 ? std::cout << this->_contact.getNickName().substr(0, 9) + '.' : std::cout << std::right << this->_contact.getNickName();//_nickName;
 	std::cout << "│";
 	return ;
 }
 
 void	Phonebook::displayChosenContact(void) {
 	std::cout << std::endl;
-	std::cout << "\t First name . . . . . " << _firstName << std::endl;
-	std::cout << "\t Last name  . . . . . " << _lastName << std::endl;
-	std::cout << "\t Nickname . . . . . . " << _nickName << std::endl;
-	std::cout << "\t Phone number . . . . " << _phoneNumber << std::endl;
-	std::cout << "\t Darkest secret . . . " << _darkestSecret << std::endl;
+	std::cout << "\t First name . . . . . " << this->_contact.getFirstName() << std::endl;//_firstName
+	std::cout << "\t Last name  . . . . . " << this->_contact.getLastName() << std::endl;
+	std::cout << "\t Nickname . . . . . . " << this->_contact.getNickName() << std::endl;
+	std::cout << "\t Phone number . . . . " << this->_contact.getPhoneNumber() << std::endl;
+	std::cout << "\t Darkest secret . . . " << this->_contact.getDarkestSecret() << std::endl;
 	return ;
 }
