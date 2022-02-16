9b251266c743e135d796ca27f913dae000219ead
Joyce Macksuele <60663410+joycemacksuele@users.noreply.github.com>
2022-02-16 14:58:11 +0100

    Update ClassPhonebook.cpp

--- cpp00/ex01/srcs/ClassPhonebook.cpp
+++ cpp00/ex01/srcs/ClassPhonebook.cpp
@@ -13,16 +13,16 @@
 #include "../includes/ClassPhonebook.hpp"
 
 /* Constructor */
-Contact::Contact(void) {
+Contact::Phonebook(void) {
 	return ;
 }
 
 /* Destructor */
-Contact::~Contact(void) {
+Contact::~Phonebook(void) {
 	return ;
 }
 
-void	Contact::add(void) {
+void	Phonebook::add(void) {
 	std::string	input;
 
 	std::cout << "\t First name . . . . . ";
@@ -48,7 +48,7 @@ void	Contact::add(void) {
  * setw: https://www.cplusplus.com/reference/iomanip/setw/
  * substr: https://www.cplusplus.com/reference/string/string/substr/
  */
-void	Contact::displayContactsList(int index) {
+void	Phonebook::displayContactsList(int index) {
 	if (_firstName.empty() && _lastName.empty() && _nickName.empty() && _phoneNumber.empty() && _darkestSecret.empty())
 		return ;
 	std::cout << std::endl << "         │";
@@ -66,7 +66,7 @@ void	Contact::displayContactsList(int index) {
 	return ;
 }
 
-void	Contact::displayChosenContact(void) {
+void	Phonebook::displayChosenContact(void) {
 	std::cout << std::endl;
 	std::cout << "\t First name . . . . . " << _firstName << std::endl;
 	std::cout << "\t Last name  . . . . . " << _lastName << std::endl;
