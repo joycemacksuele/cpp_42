2806e833e8826775b81f345581d22e402ed2f988
joycemacksuele <jfreitas@student.s19.be>
2022-02-28 07:48:36 +0100

    c02 ex00 started

---
+++ cpp02/ex00/includes/Fixed.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfreitas <jfreitas@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/17 13:39:52 by jfreitas          #+#    #+#             */
+/*   Updated: 2022/02/27 15:59:14 by jfreitas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <string>
+#include <iostream>
+#include <unistd.h>
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+
+class Fixed {
+
+	public:
+		Fixed( void );// Default constructor
+		//Fixed( void );// Copy constructor
+		// Copy assignment operator
+		~Fixed( void );// Destructor
+
+		int		getRawBits( void ) const;// returns the raw value of the fixed-point value.
+		void	setRawBits( int const raw );// sets the raw value of the fixed-point number.
+
+	/* _varNmae -> convention to remember that this variable is private */
+	private:
+		int					_number;
+		static const int	_fractionalBit = 8;
+
+};
+
+#endif
---
+++ cpp02/ex00/srcs/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfreitas <jfreitas@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/17 14:42:17 by jfreitas          #+#    #+#             */
+/*   Updated: 2022/02/27 15:28:52 by jfreitas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Fixed.hpp"
+
+int	main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
