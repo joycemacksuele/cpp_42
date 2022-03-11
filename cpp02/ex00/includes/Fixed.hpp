e4655923bf313ae8ad84dbd47a252d33673f8d2f
joycemacksuele <jfreitas@student.s19.be>
2022-03-11 06:30:22 +0100

    c02 ex00 almost doe

--- cpp02/ex00/includes/Fixed.hpp
+++ cpp02/ex00/includes/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jfreitas <jfreitas@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 13:39:52 by jfreitas          #+#    #+#             */
-/*   Updated: 2022/03/04 16:39:29 by jfreitas      ########   odam.nl         */
+/*   Updated: 2022/03/10 11:56:10 by jfreitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,25 @@
 #define YELLOW  "\033[33m"
 #define BLUE    "\033[34m"
 
+// Copy constructor:
+// When objects are copied, cpp must create a new object from an existing object.
+// Objectd can be copied by being passed as a paremeter or being returned from a function (by value),
+// or by being constructed based on another object of the same class.
+// cpp provides a compiler-defined copy constructor if you don't.
+// It is most useful to have a copy constructor if your class has raw pointer, since
+// the compiler-generated copy constructor won't copy the value/data that the pointer is point to,
+// it will copy only the pointer.
+// 
 class Fixed {
 
 	public:
-		Fixed(void);// Default constructor
-		Fixed(Fixed const &src);// Copy constructor
-		Fixed& operator=(Fixed const &src);// Copy assignment operator
+		Fixed(void);// Default (no args) constructor
+		Fixed(const Fixed &src);// Copy constructor
+		Fixed& operator=(const Fixed &src);// Copy assignment operator
 		~Fixed(void);// Destructor
 
 		int		getRawBits(void) const;// returns the raw value of the fixed-point value.
-		void	setRawBits(int const raw);// sets the raw value of the fixed-point number.
+		void	setRawBits(const int raw);// sets the raw value of the fixed-point number.
 
 	/* _varNmae -> convention to remember that this variable is private */
 	private:
--- cpp02/ex00/srcs/Fixed.cpp
+++ cpp02/ex00/srcs/Fixed.cpp
@@ -1,31 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   Fixed.cpp                                          :+:    :+:            */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                     +:+                    */
 /*   By: jfreitas <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/04 16:36:11 by jfreitas      #+#    #+#                 */
-/*   Updated: 2022/03/04 17:08:00 by jfreitas      ########   odam.nl         */
+/*   Updated: 2022/03/10 12:30:46 by jfreitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Fixed.hpp"
 
 // Default constructor that initializes the fixed-point number value to 0.
-Fixed::Fixed(void) : _number( 0 ) {
+Fixed::Fixed(void) 
+	: _number(0) {
+	// Using initialization list so the var is initialized right when the object is created.
+	// Before the body of the constructor is executed (besides being assigned to a value inside of it).
 	std::cout << "Default constructor called" << std::endl;
 	return ;
 }
 
 // Copy constructor
-Fixed::Fixed(Fixed const &src) {
-	std::cout << "Copy constructor called" << std::endl;
+//
+// Shallow copy -  Only the pointer is copied (not what it's pointing to), so both
+// objects points to the same memory (so if memery is allocated and freed, the
+// copy object will be pointing to a freed memory space).
+// Better to use when not dealing with raw pointers.
+// EX:
+//   Fixed::Fixed(const Fixed &src) : this(src) {} ?
+//   Fixed::Fixed(const Fixed &src) : _number(src._number) {}
+//
+// Deep copy - What the pointer is pointing to is copied, so each copy will have
+// its own address (each object will have a unique pointer).
+// Better to use when dealing with raw pointers.
+// EX:
+//   Fixed::Fixed(const Fixed &src) {
+//       this = new Fixed;
+//       *this = src;
+//   }
+Fixed::Fixed(const Fixed &src) {
+	std::cout << "Copy constructor called (shallow copy)" << std::endl;
 	*this = src;
 }
 
 // Copy assignment operator
-Fixed& Fixed::operator=(Fixed const &src) {
+Fixed& Fixed::operator=(const Fixed &src) {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &src) {
 		this->_number = src.getRawBits();
@@ -44,7 +64,7 @@ int		Fixed::getRawBits( void ) const {// returns the raw value of the fixed-poin
 	return this->_number;
 }
 
-void	Fixed::setRawBits( int const raw ) {// sets the raw value of the fixed-point number.
+void	Fixed::setRawBits( const int raw ) {// sets the raw value of the fixed-point number.
 	std::cout << "setRawBits member function called" << std::endl;
 	this->_number = raw;
 }
