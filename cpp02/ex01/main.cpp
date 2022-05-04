dce43c8ec3c2a85c5e9b0ebc3ad916103994f974
Joyce Freitas <jfreitas@f0r3s5.codam.nl>
2022-05-04 19:13:11 +0200

    example to be tested and understand this damn fixed pint number

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -5,13 +5,13 @@ class Fixed
 {
 public:
 	Fixed( void ): _fixed_point_value(0) {
-		if (Fixed::_verbose)
+		//if (Fixed::_verbose)
 			std::cout << "Default constructor called" << std::endl;
 		return;
 	}
 
 	Fixed( const int value ) {
-		if (Fixed::_verbose)
+		//if (Fixed::_verbose)
 			std::cout << "Constant integer constructor called" << std::endl;
 		this->_fixed_point_value = value << this->_number_of_fractional_bits;
 		return;
@@ -19,27 +19,27 @@ public:
 
 	Fixed( const float value ) {
 		int power = pow(2, this->_number_of_fractional_bits);
-		if (Fixed::_verbose)
+		//if (Fixed::_verbose)
 			std::cout << "Constant float constructor called" << std::endl;
 		this->_fixed_point_value = roundf(value * power);
 		return;
 	}
 
 	Fixed( const Fixed &src ) {
-		if (Fixed::_verbose)
+		//if (Fixed::_verbose)
 			std::cout << "Copy constructor called" << std::endl;
 		*this = src;
 		return;
 	}
 
 	~Fixed( void ) {
-		if (Fixed::_verbose)
+		//if (Fixed::_verbose)
 			std::cout << "Destructor called" << std::endl;
 		return;
 	}
 
 	Fixed	&operator=( const Fixed &rhs ) {
-		if (Fixed::_verbose)
+		//if (Fixed::_verbose)
 			std::cout << "Assignement operator called" << std::endl;
 		if (this != &rhs)
 			this->_fixed_point_value = rhs._fixed_point_value;
@@ -51,6 +51,7 @@ public:
 	}
 
 	float	toFloat( void ) const {
+		std::cout << std::endl << "_fixed_point_value =  " << this->_fixed_point_value << std::endl;
 		int		power = pow(2, this->_number_of_fractional_bits);
 		float	result = (float)this->_fixed_point_value / power;
 		return (result);
@@ -64,18 +65,18 @@ public:
 		this->_fixed_point_value = raw;
 	}
 
-	void	setVerbose( bool value ){
-		Fixed::_verbose = value;
-	}
+//	void	setVerbose( bool value ){
+//		Fixed::_verbose = value;
+//	}
 
-	bool	getVerbose( void ) {
-		return (Fixed::_verbose);
-	}
+//	bool	getVerbose( void ) {
+//		return (Fixed::_verbose);
+//	}
 
 private:
 	int					_fixed_point_value;
 	static const int	_number_of_fractional_bits = 8;
-	bool			_verbose = false;
+	//bool			_verbose = false;
 };
 
 std::ostream	&operator<<( std::ostream &ostream, const Fixed &myClass ) {
@@ -90,14 +91,24 @@ int main( void ) {
 	Fixed const b( 10 );
 	Fixed const c( 42.42f );
 	Fixed const d( b );
+	Fixed const e( 20.75f );
 	a = Fixed( 1234.4321f );
+
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
+	std::cout << "e is " << e << std::endl;
+
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "e is " << e.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+	std::cout << "e is " << e.toFloat() << " as float" << std::endl;
+
+
 	return (0);
 }
