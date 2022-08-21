d824f0b426c48eac4ebaa150259e993ab59732d4
Joyce Freitas <jfreitas@f1r6s7.codam.nl>
2022-08-21 14:22:18 +0200

    started cpp06 ex01 - need to finish cpp05

---
+++ cpp06/includes/ExplicitConversion.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ExplicitConversion.hpp                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jfreitas <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/21 11:37:43 by jfreitas      #+#    #+#                 */
+/*   Updated: 2022/08/21 14:03:07 by jfreitas      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXPLICITCONVERSION_HPP
+# define EXPLICITCONVERSION_HPP
+
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <exception>
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define BOLD    "\x1B[1m"
+
+enum ConvertTo {
+	CHAR = 0,
+	INT = 1,
+	FLOAT = 2,
+	DOUBLE = 3
+};
+
+/*class ExplicitConversion {
+	public:
+		ExplicitConversion(void);// Default (no args) constructor
+		ExplicitConversion(const std::string& name, const unsigned int& grade);// Overloaded constructor
+		ExplicitConversion(const ExplicitConversion& src);// Overloaded Copy constructor
+		ExplicitConversion& operator=(const ExplicitConversion& rhs);// Copy assignment operator
+		virtual ~ExplicitConversion(void);// Destructor
+
+		class GradeTooHighException : public std::exception {
+			virtual const char* what() const throw();
+		};
+
+
+		// getters and setters
+		const unsigned int&	getGrade() const;
+		void				setGrade(const unsigned int& grade);
+
+
+	private:
+		const std::string	_name;
+};
+
+std::ostream& operator<<(std::ostream& outputStream, const ExplicitConversion& rhs);*/
+
+#endif
---
+++ cpp06/srcs/ExplicitConversion.cpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ExplicitConversion.cpp                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jfreitas <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/21 11:35:56 by jfreitas      #+#    #+#                 */
+/*   Updated: 2022/08/21 14:21:37 by jfreitas      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ExplicitConversion.hpp>
+
+ConvertTo FindTypeToConvert(std::string argv) {
+	if (argv.size() == 1) {
+		return ConvertTo::CHAR;
+	} else if (argv.find('f') != std::string::npos) {
+		return ConvertTo::FLOAT;
+	}
+	for (auto it = argv.begin(); it != argv.end(); it++) {
+		if (std::isdigit(*it) || *it == '-' || *it == '.') {
+			if (*it == '.') {
+				return ConvertTo::DOUBLE;
+			} else {
+				return ConvertTo::INT;
+			}
+		}
+	}
+	ConvertTo::UNKNOWN;
+}
+
+char charConversion(std::string argv) {
+}
+
+charConversion <
+int main(int argc, char **argv) {
+	if (argc == 2) {
+		std::string toBeConverted;
+		
+		std::cout << "char: " << charConversion(argv[1]) << std::endl;
+		//std::cout << "int: " << intConversion(argv[1]) << std::endl;
+		//std::cout << "float: " << floatConversion(argv[1]) << std::endl;
+		//std::cout << "double: " << doubleConversion(argv[1]) << std::endl;
+	}
+	return 0
+}
+
+// C-style Casting: (data_type)expression;
+
+// Function-style Casting: data_type(expression);
+
+/* Type Conversion operators: example_cast<new_type>(expression)
+ *
+ * static_cast
+ *   - Performs all conversions allowed implicitly.
+ *   - Explicitly call a single-argument constructor or a conversion operator.
+ *   - Convert to rvalue references.
+ *   - Convert from void* to any pointer type.
+ *   - Convert any type to void, evaluating and discarding the value.
+ *   - Convert integers, floating-point values and enum types to enum types.
+ *   - Convert enum class values into integers or floating-point values.
+ *   - Obs: No checks are performed during runtime to guarantee that the object
+ *   being converted is in fact a full object of the destination type. Therefore,
+ *   it is up to the programmer to ensure that the conversion is safe.
+ *
+ * dynamic_cast
+ *   - Can only be used with pointers and references to classes (or with void*).
+ *   - Can also downcast (convert from pointer-to-base to pointer-to-derived)
+ *   polymorphic classes (those with virtual members) if -and only if- the
+ *   pointed object is a valid complete object of the target type.
+ *   - Can also perform the other implicit casts allowed on pointers: casting
+ *   null pointers between pointers types (even between unrelated classes),
+ *   and casting any pointer of any type to a void* pointer.
+ *   - Obs: When it cannot cast a pointer because it is not a complete object
+ *   of the required class it returns a null pointer to indicate the failure.
+ *
+ * const_cast
+ *   - This type of casting manipulates the constness of the object pointed by
+ *   a pointer, either to be set or to be removed.
+ *   - Obs: Removing the constness of a pointed object to actually write to it
+ *   causes undefined behavior.
+ * reinterpret_cast
+ *   - Converts any pointer type to other pointer type, even of unrelated classes.
+ *     Neither the content pointed nor the pointer type itself is checked.
+ *   - It can also cast pointers to or from integer types.
+ */
+
+// typeid: typeid (expression)
+// typeid allows to check the type of an expression
