15d7e02022bc6320eabb64e4cb9b0bf892c030d8
Joyce Freitas <joycemacksuele@outlook.com>
2022-11-17 19:21:23 +0100

    cpp07 ex02 almost done

--- cpp07/ex02/include/Array.hpp
+++ cpp07/ex02/include/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: jfreitas <jfreita@student.codam.nl>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/17 15:17:58 by jfreitas      #+#    #+#                 */
-/*   Updated: 2022/11/17 15:57:20 by jfreitas      ########   odam.nl         */
+/*   Updated: 2022/11/17 18:24:45 by jfreitas      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@
 #define ERASE_LINE    "\033[2K"
 #define JUMP_ONE_LINE "\033[1B"
 
+#include <iomanip>
 
 /* You MUST use the operator new[] to allocate memory.
  * Preventive allocation (allocating memory in advance) is forbidden.
@@ -43,38 +44,78 @@ class Array {
 
 		// Construction with no parameter: Creates an empty array.
 		Array(void) {
+			_element = new T;
+			//_element[0] = NULL;
 		}
 
 		// Parametized construction: Creates an array of n elements initialized by default.
 		Array(unsigned int n) {
-			//Tip: Try to compile int * a = new int(); then display *a
+			_element = new T[n];
+			//int size = ::size() - 1;
+			std::cout << "size = " << _element[1] << " | n = " << n << std::endl;
+			for (unsigned int i = 0; i <= n; i++) {
+				_element[i] = 0;
+				std::cout << "i = " << i << " | element[i] = " << _element[i] << std::endl;
+			}
+
+			std::cout << "JOYCE" << std::endl;
 		}
 
 		// Copy constructor
-		// Obs.: odifying the original array or its copy after copying musn’t affect the other array.
+		// Obs.: modifying the original array or its copy after copying musn’t affect the other array.
 		Array(Array const &src) {
+			*this = src; // this will call the copy assignment operator
 		}
 
 		// Assignment operator
-		// Obs.: odifying the original array or its copy after copying musn’t affect the other array.
+		// Obs.: modifying the original array or its copy after copying musn’t affect the other array.
 		Array& operator=(Array const &rhs) {
+			if (this != &rhs) {
+				/* getrirng the size of the rhs element and if that size is >=
+				* 0, then a new array with that size can be allocated and
+				* assigned to each index of the rhs element. */
+				unsigned int size = rhs.size();
+				if (size >= 0) {
+					this->_element = new T[size];
+					for (unsigned int i = 0; i <= size; i++)
+						this->_element[i] = rhs._element[i];
+				}
+				//this->_element = rhs._element;
+			}
+			return *this;
 		}
 
 		// Subscript operator
 		// Obs.: if its index is out of bounds, an std::exception is thrown.
-		T& operator[](T *index) {
+		T& operator[](unsigned int index) {
+			if (index > size() || index < 0) {
+				throw std::exception();
+			}
+			return _element[index];
 		}
 
-		// Desctuctor
+		// Destructor
 		~Array(void) {
+			delete [] this;
 		}
 
 		// Returns the number of elements in the array and musn’t modify the current instance.
 		unsigned int size() {
+			//T* elementCopy;
+			//for (elementCopy = _element; *elementCopy; elementCopy++);
+			//std::cout << "elementCopy = " << std::dec << (long)elementCopy << std::endl;
+			//std::cout << "_element = " << std::dec << (long)_element << std::endl;
+			//std::cout << "elementCopy - _element = " << std::dec << (long)(elementCopy - _element) << std::endl;
+			//return elementCopy - _element;// computation with addresses
+			unsigned int elementSize = 0;
+			for (int i = 0; _element[i]; i++) {
+				elementSize++;
+			}
+			return elementSize;
 		}
 
 	private:
-		T element;
+		T* _element;
 };
 
 #endif
--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jfreitas <jfreita@student.codam.nl>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/08 17:23:39 by jfreitas      #+#    #+#                 */
-/*   Updated: 2022/11/17 16:02:57 by jfreitas      ########   odam.nl         */
+/*   Updated: 2022/11/17 18:26:48 by jfreitas      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,20 @@ int main() {
 	std::cout << CLEAR_SCREEN;
 
 	std::cout << BOLD << BLUE << "---------------- Test with std::string array:\n" << RESET << std::endl;
-	int * a = new int();
-	std::cout << "Array before: " << *a << std::endl;
-	// It is compulsory to specify the type when declaring objects of class templates. Otherwise, the compiler will produce an error.
-	a = Array<int>(8);// Explicit instanciation
-	std::cout << "Array after: " ;
-	for (size_t i = 0; i < 8; i++) {
-		std::cout << a[i];
-	}
+	//Tip: Try to compile int * a = new int(); then display *a
+	int *a = new int();
+	std::cout << *a << std::endl;
+
+	// It is compulsory to specify the type when declaring objects of class templates.
+	// Otherwise, the compiler will produce an error.
+	Array<int> array(8);// Explicit instanciation
+	//std::cout << "Array after: " ;
+	//for (size_t i = 0; i < 8; i++) {
+	//	std::cout << array[i];
+	//}
+	
+	delete a;
 	std::cout << std::endl << std::endl;
 
 	return 0;
 }
-
-/* std::string
- *   operator[] returns a reference
- *   c_str() and data() return a pointer.
- *
- * obs.: don't use pointers to std::string, unless you determine (by measuring)
- * that string copies are the bottleneck in your application.
- */
