7cebddedf4a52bb71fe0e95d46813a0022b20cac
Joyce Freitas <joycemacksuele@outlook.com>
2022-11-26 14:52:50 +0100

    cpp08 ex02 done

--- cpp08/ex02/include/MutantStack.hpp
+++ cpp08/ex02/include/MutantStack.hpp
@@ -23,17 +23,52 @@
 #define ERASE_LINE    "\033[2K"
 #define JUMP_ONE_LINE "\033[1B"
 
+/* Ways of implementing the stack (with iteration):
+ * 1. Use a vector or deque and limit its functionality to only allow push_back
+ * and pop_back (and iterate for this exercise).
+ * 2. bButchering the original stack to create missing features (chosen here).
+ */
+
+/* Stack:
+ * - The class template acts as a wrapper to the underlying container (standard
+ *   is a deque with only a specific set of functions is provided.
+ * - The stack pushes and pops the element from the back of the underlying
+ *   container, known as the top of the stack.
+ * - The stack data structure follows the LIFO (Last In First Out) principle.
+ *   That is, the element added last will be removed first.
+ *   So if you want the element at the bottom, you must first remove all the
+ *   elements on top. This is exactly how the stack data structure works.
+ *
+ * Applications of Stack Data Structure:
+ * - To reverse a word:
+ *   Put all the letters in a stack and pop them out. Because of the LIFO order
+ *   of stack, you will get the letters in reverse order.
+ * - In compilers:
+ *   Compilers use the stack to calculate the value of expressions like
+ *   2 + 4 / 5 * (7 - 9) by converting the expression to prefix or postfix form.
+ * - In browsers:
+ *   The back button in a browser saves all the URLs you have visited previously
+ *   in a stack. Each time you visit a new page, it is added on top of the stack.
+ *   When you press the back button, the current URL is removed from the stack,
+ *   and the previous URL is accessed.
+ */
+
+/* Real template prototype of the class stack:
+template<class T, class Container = std::deque<T>>
+class stack;
+*/
+
 template<typename T>
 class MutantStack: public std::stack<T>
 {
-    public:
-        // Default (no args) constructor
-        MutantStack<T>(void) {
-            std::cout << GREEN << "MutantStack" << RESET;
-		    std::cout << " Default constructor called" << std::endl;
+	public:
+		// Default (no args) constructor
+		MutantStack<T>(void) {
+			std::cout << GREEN << "MutantStack" << RESET;
+			std::cout << " Default constructor called" << std::endl;
 		}
 
-        /* OBS.:
+		/* OBS.:
 		 * - In C++, assignment and copy construction are different because the
 		 * copy constructor initializes uninitialized memory, whereas assignment
 		 * starts with an existing initialized object.
@@ -41,41 +76,64 @@ class MutantStack: public std::stack<T>
 		 *
 		 */
 
-        // Copy constructor
-	    MutantStack<T>(MutantStack<T> const & rhs) {
-            std::cout << GREEN << "MutantStack" << RESET;
-		    std::cout << " Copy constructor called" << std::endl;
-            *this = rhs;
-        }
+		// Copy constructor
+		MutantStack<T>(MutantStack<T> const & rhs) {
+			std::cout << GREEN << "MutantStack" << RESET;
+			std::cout << " Copy constructor called" << std::endl;
+			*this = rhs;
+		}
 
-        // Assignment operator
-	    MutantStack<T> & operator=(MutantStack<T> const & rhs) {
-            std::cout << GREEN << "MutantStack" << RESET;
-		    std::cout << " Assignment operator called" << std::endl;
-            if (this != &rhs) {
-                *this = std::stack<T>::operator=(rhs);
-            }
-            return *this;
-        }
+		// Assignment operator
+		MutantStack<T> & operator=(MutantStack<T> const & rhs) {
+			std::cout << GREEN << "MutantStack" << RESET;
+			std::cout << " Assignment operator called" << std::endl;
+			if (this != &rhs) {
+				*this = std::stack<T>::operator=(rhs);
+			}
+			return *this;
+		}
 
-        // Destructor
-	    ~MutantStack<T>(void) {
-            std::cout << GREEN << "MutantStack" << RESET;
-		    std::cout << " Destructor called" << std::endl;
-        }
+		// Destructor
+		~MutantStack<T>(void) {
+			std::cout << GREEN << "MutantStack" << RESET;
+			std::cout << " Destructor called" << std::endl;
+		}
 
-	    typename std::stack<T>::container_type::iterator begin() {
-            std::cout << GREEN << "MutantStack" << RESET;
-		    std::cout << " begin() called" << std::endl;
-            return this->c.begin();
-        }
-	    typename std::stack<T>::container_type::iterator end() {         
-            std::cout << GREEN << "MutantStack" << RESET;
-		    std::cout << " end() called" << std::endl;
-            return this->c.end();
-        }
+		typename std::stack<T>::container_type::iterator begin() {
+			std::cout << GREEN << "MutantStack" << RESET;
+			std::cout << " begin() called" << std::endl;
+			/* Returns an iterator to the first element of the list.
+			 * If the list is empty the returned iterator will be equal to end().
+			 */
+			if (this->empty()) {
+				return this->c.end();
+			}
+			return this->c.begin();
+			/* c is for const:
+			 * cbegin() always return a const_iterator type
+			 * begin() may or may not, depend on whether the caller is const or not
+			 * Ex: 
+			 * mp.begin()->second = 42 would work
+			 * mp.cbegin()->second = 42 would not
+			 */
+		}
 
-	    typedef typename std::stack<T>::container_type::iterator iterator;
+		typename std::stack<T>::container_type::iterator end() {
+			std::cout << GREEN << "MutantStack" << RESET;
+			std::cout << " end() called" << std::endl;
+			/* Returns an iterator to the element following the last element
+			 * of the list.
+			 * This element acts as a placeholder; attempting to access it
+			 * results in undefined behavior.
+			 */
+			return this->c.end();
+		}
+
+		/* It gets the member type iterator of the container_type of the stack
+		 * which by default is a deque.I.e. iterator is the iterator member type
+		 * of a deque.
+		 */
+		typedef typename std::stack<T>::container_type::iterator iterator;
 };
 
 #endif
--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jfreitas <jfreita@student.codam.nl>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/08 17:23:39 by jfreitas      #+#    #+#                 */
-/*   Updated: 2022/11/25 15:16:31 by jfreitas      ########   odam.nl         */
+/*   Updated: 2022/11/26 14:52:14 by jfreitas      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,85 +18,135 @@ void	checkleaks(void) {
 	system("leaks -q executable");
 }
 
+static void printStack(std::stack<int> s) {
+	int i = 0;
+	std::stack<int> temp;
+
+	// stack s:
+	// 0
+	// 737
+	// 5
+	// 3
+	// 5
+
+	while (s.empty() == false) {
+		temp.push(s.top());//inserts the top of s at the top of temp
+		s.pop();//removes the top element of s
+	}
+
+	// stack temp:
+	// 5
+	// 3
+	// 5
+	// 737
+	// 0
+
+	while (temp.empty() == false) {
+		std::cout << "s[" << i << "] = " << temp.top() << std::endl;
+		++i;
+		temp.pop();//removes the top element of temp
+	}
+}
+
 int main(int, char**) {
-	std::cout << CLEAR_SCREEN;
+	std::cout << CLEAR_SCREEN << std::endl;
 
-/* ****************************************************************************/
-	std::cout << BOLD << BLUE << std::endl << "---------------- My tests:\n" << RESET << std::endl;
-
+	MutantStack<int> mstack;
+	std::list<int> lst;
 	std::cout << std::endl;
 
-/* ****************************************************************************/
-	std::cout << BOLD << BLUE << "---------------- Codam tests:\n" << RESET << std::endl;
+	mstack.push(5);//inserts element at the top
+	mstack.push(17);
 
-    MutantStack<int> mstack;
-    std::list<int> lst;
+	lst.push_back(5);//adds an element to the end
+	lst.push_back(17);
+
+	// mstack or lst are now:
+	// 17
+	// 5
+
+	std::cout << "mstack.top(): " << mstack.top() << std::endl;//accesses the top element
+	mstack.pop();//removes the top element
+	std::cout << "mstack.size(): " << mstack.size() << std::endl;
 	std::cout << std::endl;
 
-    mstack.push(5);
-    mstack.push(17);
-
-    lst.push_back(5);
-    lst.push_back(17);
-
-    std::cout << "mstack.top(): " << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << "mstack.size(): " << mstack.size() << std::endl;
+	std::cout << "lst.back(): " << lst.back() << std::endl;//access the last element
+	lst.pop_back();//removes the last element
+	std::cout << "lst.size(): " << lst.size() << std::endl;
 	std::cout << std::endl;
 
-    std::cout << "lst.back(): " << lst.back() << std::endl;
-    lst.pop_back();
-    std::cout << "lst.size(): " << lst.size() << std::endl;
+	// mstack or lst are now:
+	// 5
+
+	mstack.push(3);//inserts element at the top
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+	lst.push_back(3);//adds an element to the end
+	lst.push_back(5);
+	lst.push_back(737);
+	//[...]
+	lst.push_back(0);
+
+	// mstack or lst are now:
+	// 5
+	// 3
+	// 5
+	// 737
+	// 0
+
+	MutantStack<int>::iterator it = mstack.begin();//returns an iterator to the beginning
+	MutantStack<int>::iterator ite = mstack.end();//returns an iterator to the end
+	++it;
+	--it;
+
+	std::list<int>::iterator lst_it = lst.begin();//returns an iterator to the beginning
+	std::list<int>::iterator lst_ite = lst.end();//returns an iterator to the end
+	++lst_it;
+	--lst_it;
+
+	std::cout << std::endl;
+	int i = 0;
+	while (it != ite)
+	{
+		std::cout << "mstack[" << i << "] = " << *it << std::endl;
+		++it;
+		++i;
+	}
+	std::cout << "mstack.size(): " << mstack.size() << std::endl;
 	std::cout << std::endl;
 
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-
-    lst.push_back(3);
-    lst.push_back(5);
-    lst.push_back(737);
-    //[...]
-    lst.push_back(0);
-
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-
-    std::list<int>::iterator lst_it = lst.begin();
-    std::list<int>::iterator lst_ite = lst.end();
-    ++lst_it;
-    --lst_it;
-
-	std::cout << std::endl;
-    int i = 0;
-    while (it != ite)
-    {
-        //std::cout << *it << std::endl;
-        std::cout << "mstack[" << i << "] = " << *it << std::endl;
-        ++it;
-        ++i;
-    }
-    std::cout << "mstack.size(): " << mstack.size() << std::endl;
+	i = 0;
+	while (lst_it != lst_ite)
+	{
+		std::cout << "lst[" << i << "] = " << *lst_it << std::endl;
+		++lst_it;
+		++i;
+	}
+	std::cout << "lst.size(): " << lst.size() << std::endl;
 	std::cout << std::endl;
 
-    i = 0;
-    while (lst_it != lst_ite)
-    {
-        //std::cout << *lst_it << std::endl;
-        std::cout << "lst[" << i << "] = " << *lst_it << std::endl;
-        ++lst_it;
-        ++i;
-    }
-    std::cout << "lst.size(): " << lst.size() << std::endl;
+	std::cout << BLUE << "Making a copy of 'mstack' to 's' and 'lst' to 'l':" << RESET <<std::endl;
 	std::cout << std::endl;
+	std::stack<int> s(mstack);
+	std::list<int> l(lst);
 
-    std::stack<int> s(mstack);
-    std::list<int> l(lst);
-/* ****************************************************************************/
+	//it = s.begin();//error: no member begin on stack
+	//ite = s.end();//error: no member begin on stack
+	printStack(s);
+	lst_it = lst.begin();
+	lst_ite = lst.end();
+	i = 0;
+	std::cout << std::endl;
+	while (lst_it != lst_ite)
+	{
+		std::cout << "l[" << i << "] = " << *lst_it << std::endl;
+		++lst_it;
+		++i;
+	}
+	std::cout << std::endl;
 
 	atexit(checkleaks);
 	return 0;
