a6914b66c84f56f0cd1964c7a54569d8e92eb535
joycemacksuele <jfreitas@student.s19.be>
2023-06-04 18:37:13 +0200

    done

--- cpp09/ex02/include/PmergeMe.hpp
+++ cpp09/ex02/include/PmergeMe.hpp
@@ -22,7 +22,8 @@
 #include <list>
 #include <vector>
 #include <typeinfo>
-#include <stdlib.h>
+#include <cstdlib>
+#include <algorithm>
 
 #define RESET         "\033[0m"
 #define RED           "\033[31m"
@@ -44,6 +45,9 @@ public:
     ~PmergeMe();
 
     /************************** member methods ****************************/
+    void mergeInsertVector();
+    void mergeInsertList();
+
     template<typename T>
     int getContainerSize() const {
         if (std::string(typeid(T).name()).find("vector") != std::string::npos) {
@@ -53,169 +57,28 @@ public:
         }
     };
 
-    // https://iq.opengenus.org/merge-insertion-sort/
-    template<typename T>
-    void mergeInsert(int start, int end) {
-        // Getting a good division of the container where each chunk won't have more than 25 numbers
-        short chunk_size = end / 2;
-        while (1) {
-            if (chunk_size > 25) {
-                chunk_size = chunk_size / 2;
-            } else {
-                break;
-            }
-        }
-
-        // If current container size is smaller or equal than the chunk_size, use insertion sort
-        if (end - start <= chunk_size) {
-            insertionSort<T>(start, end);
-        } else {
-            int stop = (start + end) / 2;
-            // In Merge sort we split the given array (or a chunk of it) in two parts and
-            // sort them individually (using the insertion sort) -> unless the array or chunk is
-            // already small enough, then only insertion sort is faster:
-            mergeInsert<T>(start, stop);
-            mergeInsert<T>(stop + 1, end);
-            // Then we merge the both sorted halves (it is based on divide and conquer algorithm):
-            merge<T>(start, stop, end);
-        }
-    };
-
     /************************* member variables ***************************/
     static bool verbose;
+    static bool std_lib;
 
 private:
     PmergeMe();
     std::vector<int> _vector;
     std::list<int> _list;
+    short _chunk_size;
 
-    /************************** member methods ****************************/
-    template<typename T>
-    void insertionSort(int start, int stop) {
-        if (verbose) {
-            std::cout << "insertionSort with: " << typeid(T).name() << std::endl;
-        }
+    /************************** private member methods ****************************/
+    void setChunckSize(int size);
 
-        // If we are dealing with a vector (NSt3__16vectorIiNS_9allocatorIiEEEE)
-        if (std::string(typeid(T).name()).find("vector") != std::string::npos) {
-            for (int i = start; i < stop; i++) {
-                int temp_number_next = _vector[i + 1];
-                int j = i + 1;
-                while (j > start && _vector[j - 1] > temp_number_next) {
-                    _vector[j] = _vector[j - 1];
-                    j--;
-                }
-                _vector[j] = temp_number_next;
-            }
-        }
-        // If we are dealing with a list (NSt3__14listIiNS_9allocatorIiEEEE)
-        else if (std::string(typeid(T).name()).find("list") != std::string::npos) {
-            for (int i = start; i < stop; i++) {
-                int temp_number_next = 0;
-                int temp_number = 0;
-                // A list has no easy element access methods in c++98, only begin and end (and std::next is c++11)
-                // create an iterator to point to the first element of the list
-                typename T::iterator it;
-                for (it = _list.begin(); it <= i + 1; ++it) {
-                    if (it == i) {
-                        temp_number = *it;
-                    }
-                    if (it == i + 1) {
-                        temp_number_next = *it;
-                    }
-                }
+    // https://iq.opengenus.org/merge-insertion-sort/
+    void mergeInsertVector(int start, int end);
+    void mergeInsertList(int start, int end);
 
-                int j = i + 1;
-                // Shift elements to the right to make space for the current element
-                while (j > start && temp_number > temp_number_next) {
-                    for (it = _list.begin(); it <= j; ++it) {
-                        if (it == j - 1) {
-                            temp_number = *it;
-                        }
-                        if (it == j) {
-                            _list.push_back(temp_number);
-                            // *std::next(_list.begin(), j) = *std::next(_list.begin(), j - 1);
-                        }
-                    }
-                    j--;
-                }
-                for (it = _list.begin(); it <= j; ++it) {
-                    if (it == i) {
-                        _list.push_back(temp_number_next);
-                        // *std::next(_list.begin(), j) = temp_number_next;
-                    }
-                }
-            }
-        }
-    };
+    void insertionSortVector(int start, int stop);
+    void insertionSortList(int start, int stop);
 
-    template<typename T>
-    void merge(int start, int stop, int end) {
-        typename T::iterator it;
-        int number1 = stop - start + 1;
-        int number2 = end - stop;
-        T left_chunck, right_chunk;
-        // Copy elements from the left sublist to left_chunck
-        for (int i = 0; i < number1; i++) {
-            if (std::string(typeid(T).name()).find("vector") != std::string::npos) {
-                left_chunck[i] = _vector[start + i];
-            } else if (std::string(typeid(T).name()).find("list") != std::string::npos) {
-                // left_chunck.push_back(*std::next(_list.begin(), start + i));
-                for (it = _list.begin(); it <= (start + i); ++it) {
-                    if (it == (start + i)) {
-                        left_chunck.push_back(*it);
-                    }
-                }
-            }
-        }
-        // Copy elements from the right sublist to right_chunk
-        for (int i = 0; i < number2; i++) {
-            if (std::string(typeid(T).name()).find("vector") != std::string::npos) {
-                right_chunk[i] = _vector[stop + 1 + i];
-            } else if (std::string(typeid(T).name()).find("list") != std::string::npos) {
-                // right_chunk.push_back(*std::next(_list.begin(), stop + 1 + i));
-                for (it = _list.begin(); it <= (stop + 1 + i); ++it) {
-                    if (it == (stop + 1 + i)) {
-                        right_chunk.push_back(*it);
-                    }
-                }
-            }
-        }
-
-        int left_index, right_index = 0;
-        // Merge left_chunck and right_chunk into the container
-        for (int i = start; i <= end; i++) {
-            if (right_index == number2) {
-                if (std::string(typeid(T).name()).find("vector") != std::string::npos) {
-                    _vector[i] = left_chunck[left_index];
-                }
-                *std::next(_list.begin(), i) = left_chunck.front();
-                left_chunck.pop_front();
-                left_index++;
-            } else if (left_index == number1) {
-                if (std::string(typeid(T).name()).find("vector") != std::string::npos) {
-                    _vector[i] = right_chunk[right_index];
-                }
-                *std::next(_list.begin(), i) = right_chunk.front();
-                right_chunk.pop_front();
-                right_index++;
-            } else if (right_chunk.front() > left_chunck.front()) {
-                if (std::string(typeid(T).name()).find("vector") != std::string::npos) {
-                    _vector[i] = left_chunck[left_index];
-                }
-                *std::next(_list.begin(), i) = left_chunck.front();
-                left_chunck.pop_front();
-                left_index++;
-            } else {
-                if (std::string(typeid(T).name()).find("vector") != std::string::npos) {
-                    _vector[i] = right_chunk[right_index];
-                } else
-                *std::next(_list.begin(), i) = right_chunk.front();
-                right_chunk.pop_front();
-                right_index++;
-            }
-        }
-    };
+    void mergeVector(int start, int stop, int end);
+    void mergeList(int start, int stop, int end);
 };
 
 #endif
--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -13,6 +13,7 @@
 #include "PmergeMe.hpp"
 
 bool PmergeMe::verbose = false;
+bool PmergeMe::std_lib = false;
 
 PmergeMe::PmergeMe() {
     if (verbose) {
@@ -27,19 +28,17 @@ PmergeMe::PmergeMe(int argc, char** argv) {
         std::cout << " Overloaded constructor called" << std::endl;
     }
 
+    for(int j = 0; argv[1][j] != '\0'; j++) {
+        if (argv[1][j] < '0' || argv[1][j] > '9') {
+            throw std::invalid_argument("Error: bad input. Please input positive integers in between spaces.");
+        }
+    }
+
     std::cout << "Before: ";
     // Storing input numbers in both containers
     for (int i = 1; i < argc; i++) {
         try {
-            for(int j = 0; argv[i][j] != '\0'; j++) {
-                if (argv[i][j] < '0' || argv[i][j] > '9') {
-                    throw std::invalid_argument("Error: bad input. Please input positive integers in between spaces.");
-                }
-            }
             int number = atoi(argv[i]);
-            if (number <= 0) {
-                throw std::invalid_argument("Error: Please input positive integers in between spaces.");
-            }
 
             // print input before it gets sorted:
             std::cout << argv[i] << " ";
@@ -51,6 +50,18 @@ PmergeMe::PmergeMe(int argc, char** argv) {
         }
     }
     std::cout << std::endl;
+    // Setting the size of the chunks to be sorted before merging them (it will be valid for both vector and list).
+    // Getting a good division of the container where each chunk won't have more than 25 numbers:
+    setChunckSize(_vector.size());
+
+    std::vector<int> temp_vector = _vector;
+    std::sort(temp_vector.begin(), temp_vector.end());
+    // Display sorted sequence with vector
+    std::cout << "After: ";
+    for (std::vector<int>::const_iterator it = temp_vector.begin(); it != temp_vector.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl << std::endl;
 }
 
 PmergeMe::PmergeMe(PmergeMe const & rhs)
@@ -78,10 +89,202 @@ PmergeMe::~PmergeMe() {
         std::cout << GREEN << __FUNCTION__ << RESET;
         std::cout << " Destructor called" << std::endl;
     }
-    // Display sorted sequence with vector
-    std::cout << "After: ";
-    for (std::vector<int>::const_iterator it = _vector.begin(); it != _vector.end(); ++it) {
-        std::cout << *it << " ";
+}
+
+void PmergeMe::mergeInsertVector() {
+    std::clock_t start_clock = std::clock();
+    int vector_size = _vector.size();
+
+    if (std_lib) {
+        std::cout << BLUE << "std::vector: Trying with std::sort():" << RESET << std::endl;
+        std::sort(_vector.begin(), _vector.end());
+    } else {
+        mergeInsertVector(0, vector_size - 1);
     }
-    std::cout << std::endl;
+
+    std::clock_t end_vector = std::clock();
+    // Display time to process (sort) the vector in millisecond:
+    std::cout << std::flush;
+    std::cout << "Time to process a range of " << vector_size << " elements with std::vector: ";
+    std::cout << std::fixed << std::setprecision(5) << (1000.0 * (end_vector - start_clock) / CLOCKS_PER_SEC) << " ms" << std::endl;
+}
+
+void PmergeMe::mergeInsertList() {
+    std::clock_t start_clock = std::clock();
+    int list_size = _list.size();
+
+    if (std_lib) {
+        std::cout << BLUE << "std::list: Trying with .sort():" << RESET << std::endl;
+        _list.sort();
+    } else {
+        mergeInsertList(0, list_size - 1);
+    }
+
+    std::clock_t end_list = std::clock();
+    // Display time to process (sort) the list in millisecond:
+    // (https://stackoverflow.com/questions/331889/c-sorting-a-vector-or-linked-list)
+    std::cout << std::flush;
+    std::cout << "Time to process a range of " << list_size << " elements with std::vector: ";
+    std::cout << std::fixed << std::setprecision(5) << (1000.0 * (end_list - start_clock) / CLOCKS_PER_SEC) << " ms" << std::endl;
+}
+
+/************************** private member methods ****************************/
+
+void PmergeMe::mergeInsertVector(int start, int end) {
+    if (verbose) {
+        std::cout << "insertionSort with a std::vector" << std::endl;
+    }
+
+    // If current container size is smaller or equal than the chunk_size, use insertion sort
+    if (end - start <= _chunk_size) {
+        insertionSortVector(start, end);
+    } else {
+        int middle = (start + end) / 2;
+
+        // In Merge sort we split the given array (or a chunk of it) in two parts and
+        // sort them individually (using the insertion sort) -> unless the array or chunk is
+        // already small enough, then only insertion sort is faster:
+        mergeInsertVector(start, middle);
+        mergeInsertVector(middle + 1, end);
+
+        // Then we merge the both sorted halves (it is based on divide and conquer algorithm):
+        mergeVector(start, middle, end);
+    }
+}
+
+void PmergeMe::mergeInsertList(int start, int end) {
+    if (verbose) {
+        std::cout << "insertionSort with a std::list" << std::endl;
+    }
+
+    // If current container size is smaller or equal than the chunk_size, use insertion sort
+    if (end - start <= _chunk_size) {
+        insertionSortList(start, end);
+    } else {
+        int middle = (start + end) / 2;
+
+        // In Merge sort we split the given array (or a chunk of it) in two parts and
+        // sort them individually (using the insertion sort) -> unless the array or chunk is
+        // already small enough, then only insertion sort is faster:
+        mergeInsertList(start, middle);
+        mergeInsertList(middle + 1, end);
+
+        // Then we merge the both sorted halves (it is based on divide and conquer algorithm):
+        mergeList(start, middle, end);
+    }
+}
+
+void PmergeMe::setChunckSize(int size) {
+    _chunk_size = size / 2;
+    while (1) {
+        if (_chunk_size > 25) {
+            _chunk_size = _chunk_size / 2;
+        } else {
+            break;
+        }
+    }
+}
+
+void PmergeMe::insertionSortVector(int start, int middle) {
+    for (int i = start; i < middle; i++) {
+        // Get the value at index (i + 1)
+        int temp_number_next = _vector[i + 1];
+
+        int j = i + 1;
+        // Shift elements to the right to make space for the current element
+        while (j > start && _vector[j] && _vector[j - 1] > temp_number_next) {
+            _vector[j] = _vector[j - 1];
+            j--;
+        }
+        _vector[j] = temp_number_next;
+    }
+}
+
+void PmergeMe::insertionSortList(int start, int middle) {
+    for (int i = start; i < middle; i++) {
+        // Get the value at index (i + 1)
+        int temp_number_next = *std::next(_list.begin(), i + 1);
+
+        int j = i + 1;
+        // Shift elements to the right to make space for the current element
+        while (j > start && *std::next(_list.begin(), j - 1) > temp_number_next) {
+            *std::next(_list.begin(), j) = *std::next(_list.begin(), j - 1);
+            j--;
+        }
+        *std::next(_list.begin(), j) = temp_number_next;
+    }
+}
+
+void PmergeMe::mergeVector(int start, int middle, int end) {
+    int number1_index = middle - start + 1;
+    int number2_index = end - middle;
+    std::vector<int> left_chunck(number1_index);
+    std::vector<int> right_chunk(number2_index);
+
+    // Copy elements from the left sublist to left_chunck
+    for (int i = 0; i < number1_index; i++) {
+        left_chunck[i] = _vector[start + i];
+    }
+    // Copy elements from the right sublist to right_chunk
+    for (int i = 0; i < number2_index; i++) {
+        right_chunk[i] = _vector[middle + 1 + i];
+    }
+
+    int left_index = 0;
+    int right_index = 0;
+    // Merge left_chunck and right_chunk into the container
+    for (int i = start; i <= end; i++) {
+        if (right_index == number2_index) {
+            _vector[i] = left_chunck[left_index];
+            left_index++;
+        } else if (left_index == number1_index) {
+            _vector[i] = right_chunk[right_index];
+            right_index++;
+        } else if (right_chunk[right_index] > left_chunck[left_index]) {
+            _vector[i] = left_chunck[left_index];
+            left_index++;
+        } else {
+            _vector[i] = right_chunk[right_index];
+            right_index++;
+        }
+    }
+}
+
+void PmergeMe::mergeList(int start, int middle, int end) {
+    int number1_index = middle - start + 1;
+    int number2_index = end - middle;
+    std::list<int> left_chunck;
+    std::list<int> right_chunk;
+
+    // Copy elements from the left sublist to left_chunck
+    for (int i = 0; i < number1_index; i++) {
+        left_chunck.push_back(*std::next(_list.begin(), start + i));
+    }
+    // Copy elements from the right sublist to right_chunk
+    for (int i = 0; i < number2_index; i++) {
+        right_chunk.push_back(*std::next(_list.begin(), middle + 1 + i));
+    }
+
+    int left_index = 0;
+    int right_index = 0;
+    // Merge left_chunck and right_chunk into the container
+    for (int i = start; i <= end; i++) {
+        if (right_index == number2_index) {
+            *std::next(_list.begin(), i) = left_chunck.front();
+            left_chunck.pop_front();
+            left_index++;
+        } else if (left_index == number1_index) {
+            *std::next(_list.begin(), i) = right_chunk.front();
+            right_chunk.pop_front();
+            right_index++;
+        } else if (right_chunk.front() > left_chunck.front()) {
+            *std::next(_list.begin(), i) = left_chunck.front();
+            left_chunck.pop_front();
+            left_index++;
+        } else {
+            *std::next(_list.begin(), i) = right_chunk.front();
+            right_chunk.pop_front();
+            right_index++;
+        }
+    }
 }
